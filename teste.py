c560e76679b1c2583579bcb1ab2a1c1a86ee64ce
Matheus Costa <matheusoliveiracos@gmail.com>
2018-05-01 15:22:18 -0300

    Colocando testes do histórico de candidaturas

--- teste.py
+++ teste.py
@@ -1,10 +1,22 @@
-from flask_sqlalchemy import SQLAlchemy
-from decouple import config
-from flask import Flask
+# -*- coding: utf-8 -*-
+# Testes histórico de candidaturas
+# Dep. Federal
+# from data_capture.federal_deputies.candidacies_data import fetch_candidacies_data
+# data = fetch_candidacies_data()
+# for d in data:
+#     print(d)
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URI')
-app.config['SQLALCHEMY_ECHO'] = True
-db = SQLAlchemy(app)
+# Senador
+# from data_capture.federal_senate.candidacies_data import fetch_candidacies_data
+# from data_capture.federal_senate.politician_data import fetch_senators_data
+# data = fetch_senators_data()
+# for s in data:
+#     print(fetch_candidacies_data(s['civil_name']))
 
-print(db.engine.dialect.has_table(db.engine, "users"))
+# Dep. Estadual
+# from data_capture.state_deputies.dataset import fetch_dataset
+# from data_capture.state_deputies.candidacies_data import get_candidacies_data
+# dataset = fetch_dataset()
+# data = get_candidacies_data(dataset)
+# for d in data:
+#     print(d)
