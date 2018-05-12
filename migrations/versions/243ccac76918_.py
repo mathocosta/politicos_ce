f7854e018bc9a99c763de0233283d64ed1dc60e5
Matheus Costa <matheusoliveiracos@gmail.com>
2018-05-12 14:29:53 -0300

    Ajeitando o bug de não configurar o banco de dados
    Não estava configurando o banco de dados (dava erro uma tabela já
    existia), pois havia um arquivo corrompido no migrations/versions. O
    script tentava criar uma tabela que já existia. Foi removido o arquivo e
    trocado a versão de downgrade de um script.

--- migrations/versions/243ccac76918_.py
+++ migrations/versions/243ccac76918_.py
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = '243ccac76918'
-down_revision = 'f3203eb5496c'
+down_revision = None
 branch_labels = None
 depends_on = None
 
