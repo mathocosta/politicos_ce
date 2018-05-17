4a878ed2579a4d42fec42057eeecc79caab8e589
Matheus Costa <matheusoliveiracos@gmail.com>
2018-05-17 20:02:59 -0300

    Link externo das proposições dos dep. federal
        Foram feitos uns ajustes na forma de captação dos dados dos dep. federais, para pegar o link externo e status da prosição. E também foram colocados para aparecer na página (politician.html). Outro ponto foi o uso inicial de filtros de proposições.

--- data_capture/federal_deputies/fetch_proposed.py
+++ data_capture/federal_deputies/fetch_proposed.py
@@ -8,36 +8,74 @@ federal qualquer
 import requests
 from bs4 import BeautifulSoup
 
+HEAD_OPTIONS = {'accept': 'application/xml'}
 
-def get_data_from_deputie(name):
 
-    print('Obtendo as proposições do deputado...')
+def get_individual_proposition(id):
+    # Antiga API
+    # r = requests.get("http://www.camara.leg.br/SitCamaraWS/Proposicoes.asmx/ObterProposicaoPorID?IdProp={}".format(id))
 
     r = requests.get(
-        "http://www.camara.leg.br/SitCamaraWS/Proposicoes.asmx/ListarProposicoes?sigla=&numero=&ano=&datApresentacaoIni=&datApresentacaoFim=&parteNomeAutor={0}&idTipoAutor=&siglaPartidoAutor=&siglaUFAutor=&generoAutor=&codEstado=&codOrgaoEstado=&emTramitacao=".format(name))
+        "https://dadosabertos.camara.leg.br/api/v2/proposicoes/{}".format(id),
+        headers=HEAD_OPTIONS)
+    soup = BeautifulSoup(r.content, 'xml')
+
+    # return soup.proposicao.LinkInteiroTeor.text
+    return {
+        "url": soup.dados.urlInteiroTeor.text,
+        "status": soup.dados.statusProposicao.descricaoSituacao.text
+    }
+
+
+def get_data_from_deputie(name):
+    print('Obtendo as proposições do deputado...')
+
+    # Antiga API
+    # r = requests.get("http://www.camara.leg.br/SitCamaraWS/Proposicoes.asmx/ListarProposicoes?sigla=&numero=&ano=&datApresentacaoIni=&datApresentacaoFim=&parteNomeAutor={0}&idTipoAutor=&siglaPartidoAutor=&siglaUFAutor=&generoAutor=&codEstado=&codOrgaoEstado=&emTramitacao=".format(name))
+
+    payload = {
+        'autor': name,
+        'siglaTipo': ['PEC', 'PL', 'RCP', 'REM'],
+        'itens': 100,
+        'ordem': 'ASC',
+        'ordenarPor': 'id'
+    }
+    r = requests.get("https://dadosabertos.camara.leg.br/api/v2/proposicoes",
+                     params=payload, headers=HEAD_OPTIONS)
 
     soup = BeautifulSoup(r.content, 'xml')
 
-    propositions = soup.find_all('proposicao')
+    # propositions = soup.find_all('proposicao')
+    propositions = soup.find_all('proposicao_')
 
     number_of_propositions = len(propositions) - 1
-
-    print(number_of_propositions)
 
     result = list()
 
     for i in range(0, number_of_propositions):
+        prop = get_individual_proposition(propositions[i].id.text)
+
         result.append({
-            'proposition_code': propositions[i].id.text,
-            'proposition_name': propositions[i].nome.text,
-            'type': propositions[i].tipoProposicao.sigla.text,
-            'proposition_number': propositions[i].numero.text,
+            'id': propositions[i].id.text,
+            'siglum': propositions[i].siglaTipo.text,
+            'number': propositions[i].numero.text,
+            'description': propositions[i].ementa.text,
             'year': propositions[i].ano.text,
-            'proposition_date': propositions[i].datApresentacao.text,
-            'title': propositions[i].txtExplicacaoEmenta.text,
-            'description': propositions[i].txtEmenta.text,
-            'ongoing': propositions[i].situacao.descricao.text
+            'status': prop['status'],
+            'url': prop['url']
         })
+        # result.append({
+        #     'proposition_code': propositions[i].id.text,
+        #     'proposition_name': propositions[i].nome.text,
+        #     'type': propositions[i].tipoProposicao.sigla.text,
+        #     'proposition_number': propositions[i].numero.text,
+        #     'year': propositions[i].ano.text,
+        #     'proposition_date': propositions[i].datApresentacao.text,
+        #     'title': propositions[i].txtExplicacaoEmenta.text,
+        #     'description': propositions[i].txtEmenta.text,
+        #     'ongoing': propositions[i].situacao.descricao.text,
+        #     'url': url
+        # })
 
     return result
 
