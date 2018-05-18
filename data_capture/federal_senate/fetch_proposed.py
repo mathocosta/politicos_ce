a1a2c442496e4a5c9b098bcb7403ba5ec608534c
Matheus Costa <matheusoliveiracos@gmail.com>
2018-05-18 15:41:44 -0300

    Link externo para as proposições dos senadores
    Assim como nos deputados, foram
    feitos ajustes na captação dos dados
    para pegar o link do documento da
    proposição. Também foi colocado filtros na requisição dos dados.

--- data_capture/federal_senate/fetch_proposed.py
+++ data_capture/federal_senate/fetch_proposed.py
@@ -8,12 +8,45 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def get_data_from_senator(id):
+def get_proposition_url(id):
+    r = requests.get(
+        "http://legis.senado.leg.br/dadosabertos/materia/textos/{}".format(id))
 
+    soup = BeautifulSoup(r.content, 'xml')
+    texts = soup.Materia.Textos.find_all("Texto")
+
+    url = ""
+
+    for t in texts:
+        if t.DescricaoTipoTexto.text == 'Projeto de Lei':
+            url = t.UrlTexto.text
+            break
+
+    return url
+
+
+def get_props_by_type(id, prop_type):
+    """Pega as proposições de um senador por tipo.
+
+    A API de dados abertos do senado, ao contrário da câmara, não
+    permite a passagem de mais parâmetros de um tipo de uma vez
+    na requisição das propostas. Portanto foi criada essa função.
+
+
+    Args:
+        id (int): Identificador do Senador
+        prop_type (str): Sigla do tipo da proposição
+
+    Returns:
+        list: Proposições desse tipo.
+    """
     print('Obtendo os dados do senador...')
 
+    payload = {'sigla': prop_type}
+
     r = requests.get(
-        "http://legis.senado.gov.br/dadosabertos/senador/{0}/autorias".format(id))
+        "http://legis.senado.gov.br/dadosabertos/senador/{0}/autorias".format(
+            id), params=payload)
 
     soup = BeautifulSoup(r.content, 'xml')
 
@@ -21,23 +54,30 @@ def get_data_from_senator(id):
 
     number_of_propositions = len(propositions) - 1
 
-    print(number_of_propositions)
-
     result = list()
 
     for i in range(0, number_of_propositions):
+        url = get_proposition_url(propositions[i].CodigoMateria.text)
+
         result.append({
             'proposition_code': propositions[i].CodigoMateria.text,
-            'type': propositions[i].SiglaSubtipoMateria.text,
-            'proposition_number': propositions[i].NumeroMateria.text,
+            'siglum': propositions[i].SiglaSubtipoMateria.text,
+            'number': propositions[i].NumeroMateria.text,
             'year': propositions[i].AnoMateria.text,
-            'ongoing': propositions[i].IndicadorTramitando.text,
+            'status': propositions[i].IndicadorTramitando.text,
             'original_author': propositions[i].IndicadorAutorPrincipal.text,
-            'description': propositions[i].EmentaMateria.text
+            'description': propositions[i].EmentaMateria.text,
+            'url': url
         })
 
     return result
 
+def get_data_from_senator(id):
+    result = list()
+    result.extend(get_props_by_type(id, 'pls'))
+    result.extend(get_props_by_type(id, 'pec'))
+
+    return result
 
 def main():
     print(get_data_from_senator(3396))
