10f8b0ed577aa7049e6bcc31c2587b0fce178e78
Matheus Costa <matheusoliveiracos@gmail.com>
2018-05-20 17:31:06 -0300

    Usar classe para a /politician view
    Por estar ficando muitas coisas a serem processadas para a página, foi
    colocado para ficar tudo junto em uma só classe.

--- app/views.py
+++ app/views.py
@@ -5,6 +5,7 @@ da entidade específica, que está modelada no `models.py`.
 """
 import pandas as pd
 from flask import render_template, request
+from flask.views import View, MethodView
 from werkzeug.contrib.cache import SimpleCache
 
 from app import app, db
@@ -19,11 +20,13 @@ from data_capture.federal_senate.fetch_voted_propositions import \
 c = SimpleCache()
 
 
+# INDEX PAGE
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
+# SEARCH RESULTS PAGE
 @app.route('/search')
 def show_search_results():
     # FIXME: Não tem algo para verificar se o campo 'name' está vazio.
@@ -42,6 +45,7 @@ def show_search_results():
         'politician_list.html', title=title, politicians=politicians)
 
 
+# POLITICIAN LIST PAGE
 @app.route('/politician-list/<position>')
 def show_politician_list(position):
     title = ""
@@ -61,76 +65,85 @@ def show_politician_list(position):
         'politician_list.html', title=title, politicians=politicians)
 
 
-def votes_filter(df) -> dict:
-    is_secret = df.secret_poll == 'Sim'
-    is_yes = df.vote == 'Sim'
-    is_no = df.vote == 'Não'
+# INDIVIDUAL POLITICIAN PAGE
+class ShowPoliticianPage(View):
 
-    filtered_votes = dict()
-    filtered_votes['secret'] = df.loc[is_secret].to_dict('records')
-    filtered_votes['yes'] = df.loc[is_yes].to_dict('records')
-    filtered_votes['no'] = df.loc[is_no].to_dict('records')
-    # FIXME: Ver outra forma de fazer essa verificação
-    filtered_votes['abstention'] = df.loc[(df.secret_poll == 'Não')
-                                          & (df.vote != 'Sim')
-                                          & (df.vote != 'Não')
-                                          ].to_dict('records')
+    def dispatch_request(self, politician_id):
+        self.politician_data = Politician.query.get_or_404(politician_id)
+        self.position = ""
 
-    return filtered_votes
+        self.propositions, self.votes = self.fetch_external_data()
+
+        return render_template("politician.html",
+                               politician_data=self.politician_data,
+                               position=self.position,
+                               propositions=self.propositions,
+                               votes=self.votes)
+
+    def fetch_external_data(self):
+        registered_id = self.politician_data.registered_id
+        propositions = list()
+        votes = dict.fromkeys(
+            ['yes', 'no', 'abstention', 'secret'], list())
+
+        if self.politician_data.position == 'senator':
+            self.position = 'Senador'
+            propositions = self._fetch_propositions(
+                registered_id, get_props_from_senator)
+            votes = self._fetch_votes(
+                registered_id, get_votes_from_senator)
+        elif self.politician_data.position == 'federal-deputy':
+            self.position = 'Deputado Federal'
+            # FIXME: Remover isso, é pq nao pode (ainda) o id do deputado.
+            registered_id = self.politician_data.parliamentary_name
+            propositions = self._fetch_propositions(
+                registered_id, get_props_from_deputie)
+        elif self.politician_data.position == 'state-deputy':
+            self.position = 'Deputado Estadual'
+
+        return propositions, votes
+
+    def _fetch_propositions(self, registered_id, callback):
+        propositions_key = "{}-propositions".format(id)
+        propositions = c.get(propositions_key)
+
+        if propositions is None:
+            df = callback(registered_id)
+            propositions = df.to_dict('records')
+            c.set(propositions_key, propositions, timeout=86400)
+
+        return propositions
+
+    def _fetch_votes(self, registered_id, callback):
+        votes_key = "{}-votes".format(id)
+        votes = c.get(votes_key)
+
+        if votes is None:
+            df = callback(registered_id)
+            votes = df.to_dict('records')
+            c.set(votes_key, votes)
+
+        filtered_votes = self._votes_filter(pd.DataFrame(votes))
+
+        return filtered_votes
+
+    def _votes_filter(self, df):
+        is_secret = df.secret_poll == 'Sim'
+        is_yes = df.vote == 'Sim'
+        is_no = df.vote == 'Não'
+
+        filtered_votes = dict()
+        filtered_votes['secret'] = df.loc[is_secret].to_dict('records')
+        filtered_votes['yes'] = df.loc[is_yes].to_dict('records')
+        filtered_votes['no'] = df.loc[is_no].to_dict('records')
+        # FIXME: Ver outra forma de fazer essa verificação
+        filtered_votes['abstention'] = df.loc[(df.secret_poll == 'Não')
+                                              & (df.vote != 'Sim')
+                                              & (df.vote != 'Não')
+                                              ].to_dict('records')
+
+        return filtered_votes
 
 
-def get_senator_data(id, registered_id):
-    propositions_key = "{}-propositions".format(id)
-    propositions = c.get(propositions_key)
-
-    votes_key = "{}-votes".format(id)
-    votes = c.get(votes_key)
-
-    if propositions is None:
-        df = get_props_from_senator(registered_id)
-        propositions = df.to_dict('records')
-        c.set(propositions_key, propositions, timeout=86400)
-
-    if votes is None:
-        df = get_votes_from_senator(registered_id)
-        votes = df.to_dict('records')
-        c.set(votes_key, votes)
-
-    filtered_votes = votes_filter(pd.DataFrame(votes))
-
-    return propositions, filtered_votes
-
-
-def get_deputy_data(id, registered_id):
-    propositions_key = "{}-propositions".format(id)
-    propositions = c.get(propositions_key)
-
-    if propositions is None:
-        df = get_props_from_deputie(registered_id)
-        propositions = df.to_dict('records')
-        c.set(propositions_key, propositions, timeout=86400)
-
-    return propositions
-
-
-@app.route('/politician/<int:politician_id>')
-def show_politician_page(politician_id):
-    politician_data = Politician.query.get_or_404(politician_id)
-    position = ""
-    propositions = list()
-    votes = dict.fromkeys(['yes', 'no', 'abstention', 'secret'], list())
-
-    if politician_data.position == 'senator':
-        position = 'Senador'
-        propositions, votes = get_senator_data(
-            politician_id, politician_data.registered_id)
-    elif politician_data.position == 'federal-deputy':
-        position = 'Deputado Federal'
-        propositions = get_deputy_data(
-            politician_id, politician_data.parliamentary_name)
-    elif politician_data.position == 'state-deputy':
-        position = 'Deputado Estadual'
-
-    return render_template(
-        "politician.html", politician_data=politician_data,
-        position=position, propositions=propositions[:5], votes=votes)
+app.add_url_rule('/politician/<int:politician_id>',
+                 view_func=ShowPoliticianPage.as_view('show_politician_page'))
